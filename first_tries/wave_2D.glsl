7ff8576a5ced720b2bbc14ef66c7e9a74e242d58
Jonas Ward <ward@ugd.ai>
2021-01-20 19:44:51 +0100

    wave_2D_3D

---
+++ first_tries/wave_2D.glsl
@@ -0,0 +1,149 @@
+// Gyroid Marching
+
+#define MAX_STEPS 500
+#define MAX_DIST 10000.
+#define SURF_DIST .01
+
+# define GLOBALSCALE 20.
+
+# define PI 3.1415
+# define TAU 6.283185
+# define SCALE 5.0
+# define THICKNESS 1.0
+# define TIMESCALE .2
+# define SESCALE 25.
+# define TERCALE 2.
+// # define SPHERERAD 10.0
+# define BOXSIZE .5
+# define BANDHEIGHT .02
+# define DOUBLEBH .04
+# define SQUAREBH .0004
+
+# define SPHERESPACING 6.283185
+
+const float d2mid = 3.;
+const float recSpacing = 5.;
+const vec3 circle = vec3(0., d2mid, sqrt( d2mid * d2mid + recSpacing * recSpacing * .25));
+
+float rand(vec2 c){
+	return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float sdPlane(vec3 p){
+    vec4 n = vec4(0, 0, 1, -.1);
+    return dot(p,n.xyz) + n.w;
+}
+
+mat2 Rot(float a) {
+    float s = sin(a);
+    float c = cos(a);
+    return mat2(c, -s, s, c);
+}
+
+float smin(float a, float b, float k) {
+    float h = clamp(.5  + .5 * (b - a) / k, .0, 1.);
+    return mix(b, a, h) - k * h * (1.0 - h);
+}
+
+float sdCircle(vec2 p) {
+    return length(circle.xy - p) - circle.z;
+}
+
+float sdSphere(vec3 p) {
+    vec3 center = vec3(0);
+    float radius = BOXSIZE;
+    return length( (p - center) ) - radius;
+}
+
+vec2 recMod(vec2 p) {
+    return mod(p, recSpacing) - recSpacing * .5;
+}
+
+vec2 recModAlternating(vec2 p) {
+    vec2 modVec = mod(p, recSpacing);
+    vec2 cell = mod( (p - modVec) / recSpacing, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x; // * cell.y;
+    vec2 temp = vec2(modVec.xy - recSpacing * .5);
+    // return temp;
+    return multiplier * temp; // - SPHERESPACING * .5;
+}
+
+vec3 boxMod(vec3 p){
+    return mod(vec3(p), SPHERESPACING) - SPHERESPACING * .5;
+}
+
+vec3 boxModAlternating(vec3 p){
+    vec3 modVec = mod(p, SPHERESPACING);
+    vec3 cell = mod( (p - modVec) / SPHERESPACING, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x * cell.y;
+    vec3 temp = vec3(modVec.xy - SPHERESPACING * .5, modVec.z);
+    // return temp;
+    return vec3(multiplier * (temp.xy), temp.z); // - SPHERESPACING * .5;
+}
+
+float sdModSphere(vec3 p){
+    return sdSphere(boxMod(p) );
+}
+
+float sdWave(vec3 inputP) {
+    vec2 p = inputP.xy;
+    vec2 modVec = mod(p, recSpacing);
+    vec2 cell = mod( (p - modVec) / recSpacing, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x; // * cell.y;
+    vec2 temp = vec2(modVec.xy - recSpacing * .5);
+    // return temp;
+    temp *= multiplier; // - SPHERESPACING * .5;
+    return multiplier * sdCircle(temp) * cell.y;
+}
+
+float GetDist(vec3 p) {
+    // float t = iTime;
+
+    // float d_g = SCALE * sdGyroid(p, SESCALE * sdGyroid(p, sdGyroid(p, TERCALE)));
+    // float d_s = sdModSphere(p);
+    // float d_b = sdBands(p);
+    float d_p = sdPlane(p);
+    float d_rec = sdCircle(recModAlternating(p.xy));
+    // float d_s = sdCylinder(p);
+    // float d_s = sdSphere(p);
+    // intersection
+    // float d = max(d_p, d_g);
+    // float d = max(d_s, d_g) + d_b;
+    // bumping
+    // float d = d_s + .4 * d_g + d_b;
+    // float d = d_s + .4 * d_g;
+    // float d = sdGyroid(p);
+    // float d = max(d_rec, d_p);
+    float d = sdWave(p);
+
+    return d;
+}
+
+vec3 GetNormal(vec3 p) {
+	float d = GetDist(p);
+    vec2 e = vec2(.01, 0);
+    
+    vec3 n = d - vec3(
+        abs(GetDist(p-e.xyy)),
+        abs(GetDist(p-e.yxy)),
+        abs(GetDist(p-e.yyx))
+    );
+    
+    return normalize(n);
+    // return abs(normalize(n) );
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord.xy * GLOBALSCALE / iResolution.xy;
+    uv.x *= iResolution.x / iResolution.y;
+
+    vec3 p = vec3(uv, 0.);
+
+    float ds = GetDist(p) * SCALE;
+    vec3 n = GetNormal(p);
+
+    // float ds = abs(dot(sin(p), cos(p.yzx) ) );
+
+    fragColor = vec4(vec3(n),1.);
+}
---
+++ first_tries/wave_voxelizer.glsl
@@ -0,0 +1,219 @@
+#define MAX_STEPS 100
+#define MAX_DIST 200.
+#define SURF_DIST .0001
+
+# define PI 3.1415
+# define TAU 6.283185
+# define SCALE .2
+# define THICKNESS 1.0
+# define TIMESCALE .2
+# define SESCALE 2.
+# define TERCALE .512542
+// # define SPHERERAD 10.0
+# define BOXSIZE .5
+
+const vec4 b_pln = vec4(.0, 0., 1., 19.1);
+const vec4 b_pln_ref = vec4(.0, 0., 1., 11.);
+
+const float bandHeight = .05;
+const float doubleBH = 2. * bandHeight;
+const float squareBH = bandHeight * bandHeight;
+
+const float voxScale = .25;
+const float voxScaleMult = 1. / voxScale;
+
+const float d2mid = 10.;
+const float recSpacing = 5.;
+const vec3 circle = vec3(0., d2mid, sqrt( d2mid * d2mid + recSpacing * recSpacing * .25));
+
+float sdPlane(vec3 p, vec4 pln){
+    return dot(p,pln.xyz) + SCALE * pln.w;
+}
+
+mat2 Rot(float a) {
+    float s = sin(a);
+    float c = cos(a);
+    return mat2(c, -s, s, c);
+}
+
+float smin(float a, float b, float k) {
+    float h = clamp(.5  + .5 * (b - a) / k, .0, 1.);
+    return mix(b, a, h) - k * h * (1.0 - h);
+}
+
+float sdBands(vec3 p) {
+    float val = mod(p.z, doubleBH);
+    val -= bandHeight;
+    val = sqrt(squareBH - val * val);
+    return -val;
+}
+
+float sdGyroid(vec3 p, float scale) {
+    p *= scale;
+    float d = dot(sin(p), cos(p.yzx) );
+    // float d = abs(dot(sin(p), cos(p.yzx) ) + THICKNESS) ;
+    // float d = abs(dot(sin(p), cos(p.yzx))+bias)-thickness;
+    // d += 3.0;
+    d *= .3333;
+	return d;
+}
+
+float sdCircle(vec2 p) {
+    return length(circle.xy - p) - circle.z;
+}
+
+// vec3 voxelizer(vec3 p) {
+//     float vScale = (sdGyroid(p, SCALE) + 2.) * voxScale;
+//     // return voxScale * round(p * voxScaleMult);
+//     return vScale * round(p / vScale);
+//     // return vec3(round(p.x), round(p.y), round(p.z));
+// }
+
+float sdWaveX(vec3 inputP) {
+    vec2 p = inputP.xy;
+    vec2 modVec = mod(p, recSpacing);
+    vec2 cell = mod( (p - modVec) / recSpacing, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x; // * cell.y;
+    vec2 temp = vec2(modVec.xy - recSpacing * .5);
+    // return temp;
+    temp *= multiplier; // - SPHERESPACING * .5;
+    return multiplier * sdCircle(temp) * cell.y;
+}
+
+float sdWaveY(vec3 inputP) {
+    vec2 p = inputP.yx;
+    vec2 modVec = mod(p, recSpacing);
+    vec2 cell = mod( (p - modVec) / recSpacing, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x; // * cell.y;
+    vec2 temp = vec2(modVec.xy - recSpacing * .5);
+    // return temp;
+    temp *= multiplier; // - SPHERESPACING * .5;
+    return -multiplier * sdCircle(temp) * cell.y;
+}
+
+float sdWaveZ(vec3 inputP) {
+    vec2 p = inputP.yz;
+    vec2 modVec = mod(p, recSpacing);
+    vec2 cell = mod( (p - modVec) / recSpacing, 2.0) * 2.0 - 1.0;
+    float multiplier = cell.x; // * cell.y;
+    vec2 temp = vec2(modVec.xy - recSpacing * .5);
+    // return temp;
+    temp *= multiplier; // - SPHERESPACING * .5;
+    return multiplier * sdCircle(temp) * cell.y;
+}
+
+float GetDist(vec3 p) {
+    // float d = sdPlane(p, b_pln_ref);
+    // float d_0 = sdPlane(p, b_pln);
+
+    // if (d > 0.) {
+    //     return d_0;
+    // } else {
+        float waveDx = sdWaveX(p);
+        float waveDy = sdWaveY(p);
+        float waveDz = sdWaveZ(p);
+        float waveD = max(max(waveDx, waveDy), waveDz);
+        // float waveD = max(waveDx, waveDy);
+
+        return waveD;
+    //     return max(d_0, waveD);
+    // }
+
+    // float d_g = sdGyroid(p, SCALE * sdGyroid(p, SESCALE) );
+    // return min(d, 100.);
+
+    // return d_g + d;
+}
+
+float RayMarch(vec3 ro, vec3 rd) {
+    float d0 = 0.;
+
+    for (int i = 0; i < MAX_STEPS; i++) {
+        vec3 p = ro + rd * d0;
+        float dS = GetDist(p);
+        d0 += dS;
+
+        if (d0 > MAX_DIST || dS < SURF_DIST) break;
+    }
+
+    return d0;
+}
+vec3 GetNormal(vec3 p) {
+	float d = GetDist(p);
+    vec2 e = vec2(.01, 0);
+    
+    vec3 n = d - vec3(
+        GetDist(p-e.xyy),
+        GetDist(p-e.yxy),
+        GetDist(p-e.yyx)
+    );
+    
+    return -normalize(n);
+    // return abs(normalize(n) );
+}
+
+vec3 R(vec2 uv, vec3 p, vec3 l, float z, vec3 up) {
+    vec3 f = normalize(l-p),
+        r = normalize(cross(up, f)),
+        u = cross(f,r),
+        c = p+f*z,
+        i = c + uv.x*r + uv.y*u,
+        d = normalize(i-p);
+    return d;
+}
+
+float GetLight(vec3 p) {
+    vec3 lightPos = vec3(0,0,0);
+    vec3 l = normalize(lightPos - p);
+    vec3 n = GetNormal(p);
+
+    float dif = clamp( dot(n, l) * .5 + .5, .0, 1.);
+    float d = RayMarch(p + n * SURF_DIST * 2., l);
+    if (p.y < .01 && d < length(lightPos - p)) dif *= .5;
+
+    return dif;
+}
+
+vec3 R(vec2 uv, vec3 p, vec3 l, float z) {
+    vec3 f = normalize(l - p),
+        r = normalize(cross(vec3(0, 1, 0), f)),
+        u = cross(f, r),
+        c = p + f * z,
+        i = c + uv.x * r + uv.y * u,
+        d = normalize(i - p);
+    return d;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord - .5 * iResolution.xy) / iResolution.y;
+    vec2 m = iMouse.xy/ iResolution.xy;
+
+    // vec3 col = vec3(GetNormal(fragCoord) );
+
+    vec3 ro = vec3(10., 0, 0.);
+    ro.yz *= Rot(-m.y + .4);
+    ro.xz *= Rot(5.3 + m.x * TAU);
+
+    vec3 rd = R(uv, ro, vec3(0), .58);
+
+    float d = RayMarch(ro, rd);
+    vec3 p = ro + d*rd;
+    
+    vec3 n = vec3(.5) - GetNormal(p) * .5;
+    // vec3 n2 = vec3( (abs(n.x) + abs(n.y) + abs(n.z) ) * .33333 );
+    // vec3 n2 = n;
+    // vec3 col = vec3( n2 );
+
+    // if (d < MAX_DIST) {
+    //     vec3 p = ro + rd * d;
+
+    //     float dif = GetLight(p);
+    //     col = vec3(dif);
+    // }
+
+
+    // col = pow(col, vec3(1.35));    // gamma correction
+
+    fragColor = vec4(n, 1.);
+}
